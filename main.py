fea8943841132c14dfbe4d2947b1ee47752194d7
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 00:05:55 +0200

    task 6 and polish

--- main.py
+++ main.py
@@ -3,14 +3,11 @@
 # they don’t, print the mismatched line from each file.
 
 def task1(file1, file2):
+    with open(file1, "r") as file_handler:
+        text1 = file_handler.readlines()
 
-    file_handler = open(file1, "r")
-    text1 = file_handler.readlines()
-    file_handler.close()
-
-    file_handler = open(file2, "r")
-    text2 = file_handler.readlines()
-    file_handler.close()
+    with open(file2, "r") as file_handler:
+        text2 = file_handler.readlines()
 
     for i in range(len(text1)-1):
         text1[i] = text1[i].rstrip("\n")
@@ -27,7 +24,7 @@ def task1(file1, file2):
     if error_counter == 0:
         print("No difference has been found.")
 
-# task1("Task1_1.txt", "Task1_2.txt")
+task1("Task1_1.txt", "Task1_2.txt")
 
 
 # Task 2
@@ -39,9 +36,8 @@ def task1(file1, file2):
 # ■ Number of digits.
 
 def task2(file):
-    file_handler = open(file, "r")
-    text = file_handler.readlines()
-    file_handler.close()
+    with open(file, "r") as file_handler:
+        text = file_handler.readlines()
 
     for i in range(len(text) - 1):
         text[i] = text[i].rstrip("\n")
@@ -77,7 +73,7 @@ def task2(file):
     file_handler.write(f"digits: {digits}")
     file_handler.close()
 
-#task2("Task2.txt")
+task2("Task2.txt")
 
 """Task3
 Task 3
@@ -85,24 +81,23 @@ You have a text file. Delete the last line from it. Write
 the result to another file. """
 
 def task3(file):
-    file_handler = open(file, "r")
-    text = file_handler.readlines()
-    file_handler.close()
+    with open(file, "r") as file_handler:
+        text = file_handler.readlines()
 
     file_handler = open("Task3_output.txt", "w")
     for i in range(len(text)-1):
         file_handler.writelines(text[i])
     file_handler.close()
 
-#task3("Task3-6.txt")
+task3("Task3-6.txt")
+
 
 """Task 4
 You have a text file. Find the length of the longest line."""
 
 def task4(file):
-    file_handler = open(file, "r")
-    text = file_handler.readlines()
-    file_handler.close()
+    with open(file, "r") as file_handler:
+        text = file_handler.readlines()
 
     counter = 0
     max_count = 0
@@ -118,33 +113,68 @@ def task4(file):
 
     return(max_count)
 
-# print(f"The longest line is {task4("Task3-6.txt")} characters long.")
+print(f"The longest line is {task4("Task3-6.txt")} characters long.")
 
 """ Task 5
 You have a text file. Count how many times the word
 specified by the user occurs in it."""
 
 user_input = input("Input the searched word: ")
+
 def task5(file, word):
-    file_handler = open(file, "r")
-    text = file_handler.read()
-    file_handler.close()
+    with open(file, "r") as file_handler:
+        text = file_handler.read()
 
     wordlist = text.rsplit()
     for i in range(len(wordlist)):
         wordlist[i] = wordlist[i].rstrip('.,"')
     for i in range(len(wordlist)):
         wordlist[i] = wordlist[i].lower()
-
-    print(wordlist)
-
+    word = word.lower()
 
     counter = 0
     for i in wordlist:
         if i == word:
             counter += 1
 
-    return f"Your word has been found {counter} times."
+    return f"The word '{word}' has been found {counter} times."
 
 print(task5("Task3-6.txt", user_input))
 
+"""Task 6
+You have a text file. Find and replace the specified word.
+The user determines what to search for and to what it should
+be replaced with."""
+
+input_old_word = input("Input a word which you want to replace: ")
+input_new_word = input("Input a word which you want to insert: ")
+
+
+def task6(file, old_word=input_old_word, new_word=input_new_word):
+    with open(file, "r") as file_handler:
+        text = file_handler.read()
+
+    wordlist = text.rsplit()
+
+    upper_new_word = new_word.capitalize()
+    lower_new_word = new_word.lower()
+
+    for i in range(len(wordlist)):
+        if old_word.lower() in wordlist[i].lower():
+            if wordlist[i].islower():
+                if not wordlist[i][-1].isalpha():
+                    wordlist[i] = lower_new_word + wordlist[i][-1]
+                else:
+                    wordlist[i] = lower_new_word
+
+            if wordlist[i][0].isupper():
+                if not wordlist[i][-1].isalpha():
+                    wordlist[i] = upper_new_word + wordlist[i][-1]
+                else:
+                    wordlist[i] = upper_new_word
+
+    with open("Task6_output.txt", "w") as file_handler:
+        for i in wordlist:
+            file_handler.write(f"{i} ")
+
+task6("Task3-6.txt")
---
+++ test.py
@@ -0,0 +1,19 @@
+"""str = "adjective6"
+print(str[-1].isdigit())
+print(str[0].isupper())
+print(str+".")
+capstr = str.capitalize()
+print(capstr)
+"""
+var = "Strange"
+
+word = "strange"
+
+def foo():
+
+    if word in var.lower():
+
+        return True
+    return False
+
+print(foo())
